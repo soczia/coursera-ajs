e2e56985f88eff34cdd9c939eadffcef202168c9
Sasha <sasha@pro.local>
2018-03-31 20:43:37 +0200

    module1 fixed

--- module1solution/app.js
+++ module1solution/app.js
@@ -8,20 +8,37 @@ LunchChecker.$inject = ['$scope'];
 function LunchChecker($scope) {
   $scope.menu = "";
   $scope.message = "";
-
+  $scope.emptyInfo = ""
   $scope.checkLunch = function () {
-    if ($scope.menu == "")
+    var myarray = $scope.menu.split(',');
+    console.log(myarray);
+    if ($scope.menu == "" || $scope.menu.split(',').map(item => item.trim())
+    .filter(Boolean).length == 0)
     {
       $scope.message = "Please enter data first";
+      $scope.emptyInfo = ""
+
+    if ($scope.menu != "") {
+        $scope.emptyInfo = "It do NOT consider and empty item, as an item towards to the count";
     }
-    else if ($scope.menu.split(',')
+  }
+    else if (myarray
     .map(item => item.trim())
     .filter(Boolean).length > 3)
     {
       $scope.message = "Too much!";
+      $scope.emptyInfo = ""
     }
     else {
       $scope.message = "Enjoy!";
+
+      if (myarray.length > myarray
+      .map(item => item.trim())
+      .filter(Boolean).length)
+      $scope.emptyInfo = "It do NOT consider and empty item, as an item towards to the count";
+      else {
+        $scope.emptyInfo = "";
+      }
     }
   }
 }
